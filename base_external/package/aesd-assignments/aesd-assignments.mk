80803f7f5a7979bf6d3d79db491e7424d25cb602
SagaXSaga <f74106238@gs.ncku.edu.tw>
2023-05-29 01:19:05 +0800

    HW4 part 2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'e7d7df5'
+AESD_ASSIGNMENTS_VERSION = '115789205a5e0a10cc0a5fe1f45d236aa563166f'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
